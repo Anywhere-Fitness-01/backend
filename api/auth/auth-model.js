3bcc8eb14e115142d80298b84589c5a0b276acd2
bugsbrog <bugsbrog@gmail.com>
2022-02-28 15:50:39 -0700

    Done with auth-model.js I think. Let's see if it works

---
+++ api/auth/auth-model.js
@@ -0,0 +1,21 @@
+const db = require('.././data/db-config')
+
+function find() {
+    return db('users')
+}
+
+function findBy(filter) {
+    return db('users')
+        .where(filter)
+}
+
+async function add(newUser) {
+   const [id] = await db('users').insert(newUser, ['username', 'user_id', 'password', 'role'])
+   return id
+}
+
+module.exports = {
+    find,
+    findBy,
+    add
+}
