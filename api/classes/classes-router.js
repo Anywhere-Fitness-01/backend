4066b5555ba6c5246521c08710b5ca06c8cda520
bugsbrog <bugsbrog@gmail.com>
2022-02-28 21:08:14 -0700

    Added a classesRouter in server.js

--- api/classes/classes-router.js
+++ api/classes/classes-router.js
@@ -3,7 +3,12 @@ const restricted = require('./classes-middleware')
 const Class = require('./classes-model')
 
 router.get('/', async (req, res, next) => {
-
+    try {
+        const getClasses = await Class.findClass()
+        res.json(getClasses)
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.post('/', async (req, res, next) => {
