4b035e15b4bbd4a313a2ac93332deea6c850ec9c
bugsbrog <bugsbrog@gmail.com>
2022-02-28 21:06:10 -0700

    Made mw folder and file

---
+++ api/classes/classes-middleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../secrets/index')
+
+const restricted = (req, res, next) => {
+    const token = req.headers.authorization
+        if (token) {
+            jwt.verify(token, JWT_SECRET, (err, decoded) => {
+                if (err) {
+                    next({
+                        status: 401,
+                        message: 'Token invalid'
+                    })
+                } else {
+                    req.decodedJwt = decoded
+                    next()
+                }
+            })
+        } else {
+            next({
+                status: 401,
+                message: 'Token required'
+            })
+        }
+}
+
+module.exports = restricted
---
+++ api/classes/classes-router.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+const restricted = require('./classes-middleware')
+const Class = require('./classes-model')
+
+router.get('/', async (req, res, next) => {
+
+})
+
+router.post('/', async (req, res, next) => {
+
+})
+
+router.put('/:class_id', async (req, res, next) => {
+
+})
+
+router.delete('/:class_id', async (req, res, next) => {
+
+})
+
+module.exports = router
